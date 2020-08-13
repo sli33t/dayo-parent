b074c898abbbd4ec6a676f0b31950bf6a4b09fef
linbin <sli33t@163.com>
2020-08-13 08:22:00 +0800

    增加 bootstrap.properties

--- dayo_example/src/main/java/cn/caishen/example/DayoExampleApplication.java
+++ dayo_example/src/main/java/cn/caishen/example/DayoExampleApplication.java
@@ -6,10 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class DayoExampleApplication 
-{
-    public static void main( String[] args )
-    {
+public class DayoExampleApplication {
+    public static void main( String[] args ) {
     	SpringApplication.run(DayoExampleApplication.class, args);
     }
 }
